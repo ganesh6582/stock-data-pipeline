7ec0aa94b9f1e9216a6260c14908dcb3285f9751
Ganesh Kumar <jayaveeraganesh@gmail.com>
2026-04-21 17:40:14 +0530

    Removed empty text file and added automated athena table repair

--- lambda_function.py
+++ lambda_function.py
@@ -1,14 +1,10 @@
 import json
-
 import requests
-import pandas as pd
 import boto3
 from datetime import datetime
 import io
 
 bucket_name = "de-stock-data-ganesh"
-
-
 
 def fetch_data(url):
     headers = {
@@ -27,28 +23,47 @@ def extract_timestamps_and_prices(data):
     
 def save_bronze_data(data, bucket_name, file_name, stock_name):
     s3 = boto3.client('s3')
-    json_buffer = io.StringIO()
     timestamps, prices = extract_timestamps_and_prices(data)
-    df_raw = pd.DataFrame({'timestamp': timestamps, 'price': prices, 'stock': stock_name})
-    df_raw['timestamp'] = pd.to_datetime(df_raw['timestamp'], unit='s')
-    df_raw.to_json(json_buffer, orient='records', lines=True)
-    s3.put_object(Bucket=bucket_name, Key=file_name, Body=json_buffer.getvalue())
+
+    lines = []
+    for ts, price in zip(timestamps, prices):
+        lines.append(json.dumps({
+            "timestamp": datetime.fromtimestamp(ts).isoformat(),
+            "price": price,
+            "stock": stock_name
+        }))
+
+    json_data = "\n".join(lines)
+
+    s3.put_object(Bucket=bucket_name, Key=file_name, Body=json_data, ContentType='application/json')
     return f"s3://{bucket_name}/{file_name}"
 
 def process_data(data, stock_name, now):
     timestamps, prices = extract_timestamps_and_prices(data)
-    df = pd.DataFrame({'timestamp': timestamps, 'price': prices, 'stock': stock_name})
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-    df['ingestion_date'] = now.date()
-    df = df.dropna()
-    return df
+    rows = []
+    for ts, price in zip(timestamps, prices):
+        if price is not None:
+            rows.append(f"{datetime.fromtimestamp(ts)},{price},{stock_name},{now.date()}")
+    csv_data = "timestamp,price,stock,ingestion_date\n" + "\n".join(rows)
+    return csv_data
 
-def save_to_s3(df, bucket_name, file_name):
+def save_to_s3(csv_data, bucket_name, file_name):
     s3 = boto3.client('s3')
-    csv_buffer = io.StringIO()
-    df.to_csv(csv_buffer, index=False)
-    s3.put_object(Bucket=bucket_name, Key=file_name, Body=csv_buffer.getvalue())
+    s3.put_object(Bucket=bucket_name, Key=file_name, Body=csv_data, ContentType='text/csv')
     return f"s3://{bucket_name}/{file_name}"
+
+def repair_athena_table():
+    client = boto3.client('athena')
+
+    query = "MSCK REPAIR TABLE stock_prices;"
+
+    client.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={'Database': 'stock_db'},
+        ResultConfiguration={
+            'OutputLocation': 's3://de-stock-data-ganesh/athena-results/'
+        }
+    )
 
 STOCK_LIST = ["RELIANCE.NS", "TCS.NS", "INFY.NS", "HDFCBANK.NS", "ICICIBANK.NS"]
 
@@ -62,7 +77,7 @@ def main():
 
     processed_data_file_name = f"processed/year={year}/month={month}/day={day}/stock_data_{now.strftime('%Y%m%d%H%M%S')}.csv"
     # create empty list to store dataframes for each stock
-    dfs = []
+    all_rows = []
 
     # Fetch and move raw data to S3, then process and combine data for all stocks
     for stock in STOCK_LIST:
@@ -74,17 +89,22 @@ def main():
             continue
         raw_data_file_name = f"raw/year={year}/month={month}/day={day}/{stock}_{now.strftime('%Y%m%d%H%M%S')}.json"
         s3_path = save_bronze_data(data, bucket_name, raw_data_file_name, stock.split('.')[0])
-        df = process_data(data, stock.split('.')[0], now)  # Use the stock name without the .NS suffix
+        csv_data = process_data(data, stock.split('.')[0], now)  # Use the stock name without the .NS suffix
         print("Processed data for:", stock)
-        dfs.append(df)
+        # remove header except first time
+        lines = csv_data.split("\n")
+    
+        if not all_rows:
+            all_rows.extend(lines)  # include header
+        else:
+            all_rows.extend(lines[1:])  # skip header
 
-    if not dfs:
+    if not all_rows:
         raise Exception("No data fetched")
     print("Saving combined data to S3...")
-    combined_df = pd.concat(dfs, ignore_index=True)
-    print("Combined DataFrame shape:", combined_df.shape)
-    s3_path = save_to_s3(combined_df, bucket_name, processed_data_file_name)
-    print(f"Data saved to: {s3_path}")
+    final_csv = "\n".join(all_rows)
+    save_to_s3(final_csv, bucket_name, processed_data_file_name)
+    repair_athena_table()
 
 def lambda_handler(event, context):
     main()
